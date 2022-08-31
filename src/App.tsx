ab87a013f1aef601a8475371a9405ef78f90fdec
SeeYou1ater <Sergeyharechkov8@mail.ru>
2022-08-31 15:10:38 +0300

    initialize new react-app with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface ITodo {
+  id: string
+  text: string
+  completed: boolean
+}
+
 function App() {
+
+  const [todos, setTodos] = useState<ITodo[]>([])
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos, 
+        {
+        id: new Date().toISOString(),
+        text,
+        completed: false
+        }
+      ])
+      setText('')
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        <input type="text" value={text} onChange={ (e) => { setText(e.currentTarget.value) }}/>
+        <button onClick={addTodo}>Add todo</button>
+      </label>
+      <ul>
+        {
+          todos.map( todo => <li key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <span style={{color: 'red'}}>&times;</span>
+            </li> )
+        }
+      </ul>
     </div>
   );
 }
